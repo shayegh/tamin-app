61ad130454a4e9fbe92a665c029e856859d15141
mojtaba Shayegh <Shayegh@gmail.com>
2019-10-28 14:37:07 +0330

    Added Datepicker & CreateAntField

--- polling-app-client/src/common/components/InputField.jsx
+++ polling-app-client/src/common/components/InputField.jsx
@@ -4,16 +4,16 @@ import {Form, Input} from "antd";
 const FormItem = Form.Item;
 
 export const InputField = ({
-                               field, // { name, value, onChange, onBlur }
-                               form: {touched, errors}, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-                               ...props
+                             field, // { name, value, onChange, onBlur }
+                             form: {touched, errors}, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+                             ...props
                            }) => {
-    const errorMsg = touched[field.name] && errors[field.name];
-
-    return (
-        <FormItem labelCol={{span: 12, offset: 12}} className="align-left" help={errorMsg} validateStatus={errorMsg ? "error" : 'success'} hasFeedback
-                  required={props.required} label={props.placeholder}>
-            <Input {...field} {...props} />
-        </FormItem>
-    );
+  const errorMsg = touched[field.name] && errors[field.name];
+  return (
+      <FormItem labelCol={{span: 12, offset: 12}} help={errorMsg} validateStatus={errorMsg ? "error" : 'success'}
+                hasFeedback
+                required={props.required} label={props.placeholder}>
+        <Input {...field} {...props} />
+      </FormItem>
+  );
 };
--- polling-app-client/src/testAntd.jsx
+++ polling-app-client/src/testAntd.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import {Button, Col, Form, Icon, Row, Select} from "antd";
 import {Field as FormikField, Form as FormikForm, withFormik} from "formik";
 import * as yup from "yup";
-
+import DatePicker from 'react-datepicker2';
+import moment from 'moment-jalaali';
 import "./index.css";
 import {InputField} from "./common/components/InputField";
+import {AntInput} from "./common/components/CreateAntFields";
 
 const FormItem = Form.Item;
 const Option = Select.Option;
@@ -17,58 +19,80 @@ const InnerForm = ({
                      setFieldTouched,
                      setFieldValue,
                      isSubmitting,
-                     handleSubmit
+                     handleSubmit,
+                     submitCount
                    }) => {
   return (
-      <FormikForm onSubmit={handleSubmit} >
+      <FormikForm onSubmit={handleSubmit}>
         <Row gutter={16}>
           <Col span={12}>
             <FormikField
                 name="email"
-                placeholder="ایمیل"
+                type='email'
+                label="ایمیل"
                 prefix={
                   <Icon type="user" style={{color: "rgba(0,0,0,.25)"}}/>
                 }
                 required={true}
-                component={InputField}
+                hasFeedback
+                component={AntInput}
+
             />
           </Col>
           <Col span={12}>
             <FormikField
                 name="username"
+                type='text'
                 placeholder="نام"
+                label='نام'
                 required={true}
-                component={InputField}
+                hasFeedback
+                component={AntInput}
             />
           </Col>
         </Row>
-        <FormItem>
-          <FormikField
-              name="fruit"
-              render={({field}) => (
-                  <Select
-                      {...field}
-                      onChange={value => setFieldValue("fruit", value)}
-                      onBlur={() => setFieldTouched("fruit", true)}
-                      value={values.fruit}
-                  >
-                    <Option key={1} value="Apple">
-                      Apple
-                    </Option>
-                    <Option key={2} value="Orange">
-                      Orange
-                    </Option>
-                    <Option key={3} value="Mango">
-                      Mango
-                    </Option>
-                    <Option key={4} value="Pineapple">
-                      Pineapple
-                    </Option>
-                  </Select>
-              )}
-          />
-        </FormItem>
-        <FormItem>
+        <Row gutter={16}>
+          <Col span={12}>
+            <FormItem label='میوه' labelCol={{span: 12, offset: 12}}>
+              <FormikField
+                  name="fruit"
+                  render={({field}) => (
+                      <Select
+                          {...field}
+                          onChange={value => setFieldValue("fruit", value)}
+                          onBlur={() => setFieldTouched("fruit", true)}
+                          value={values.fruit}
+                      >
+                        <Option key={1} value="Apple">
+                          Apple
+                        </Option>
+                        <Option key={2} value="Orange">
+                          Orange
+                        </Option>
+                        <Option key={3} value="Mango">
+                          Mango
+                        </Option>
+                        <Option key={4} value="Pineapple">
+                          Pineapple
+                        </Option>
+                      </Select>
+                  )}
+              />
+            </FormItem>
+          </Col>
+          <Col span={12}>
+            <FormItem label='تاریخ' labelCol={{span: 12, offset: 12}}>
+              <FormikField
+                  name='svdate'
+                  render={({field}) => (
+                      <DatePicker {...field} value={values.svdate} isGregorian={false}  className='ant-input'
+                                  inputJalaaliFormat="jYYYY/jMM/jDD" timePicker={false}/>
+                  )}
+              />
+            </FormItem>
+          </Col>
+        </Row>
+        <FormItem >
           <Button htmlType="submit" type="primary" disabled={isSubmitting}>
             Submit
           </Button>
@@ -78,16 +102,18 @@ const InnerForm = ({
 };
 
 const MyFormikForm = withFormik({
-  mapPropsToValues({username, fruit, email}) {
+  mapPropsToValues({username, fruit, email, svdate}) {
     return {
       username: username || "",
       fruit: fruit || "",
-      email: email || ""
+      email: email || "",
+      svdate: moment()
     };
   },
   validationSchema: yup.object().shape({
     username: yup.string().required('Username is required'),
     email: yup.string().required('ورود ایمیل اجباری است').email('Enter a valid Email'),
+    svdate: yup.string().required('فیلد تاریخ اجباری است')
   }),
   handleSubmit(values, {resetForm, setErrors, setSubmitting}) {
     setTimeout(() => {
