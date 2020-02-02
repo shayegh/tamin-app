0a8c4a229a299227e5072f31bfd97f4ac1d4f053
mojtaba Shayegh <Shayegh@gmail.com>
2020-02-02 08:59:13 +0330

    تبدیل APP به فانکشنال

--- tamin-app-client/src/app/App.js
+++ tamin-app-client/src/app/App.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Route, Switch, withRouter} from 'react-router-dom';
+import {Route, Switch, useHistory, withRouter} from 'react-router-dom';
 
 import {getCurrentUser} from '../util/api';
 import {ACCESS_TOKEN} from '../constants';
@@ -9,7 +9,7 @@ import Profile from '../user/profile/Profile';
 import AppHeader from '../common/AppHeader';
 import PrivateRoute from '../common/PrivateRoute';
 import {toast} from 'react-toastify';
-import {Layout, notification} from 'antd';
+import {Layout} from 'antd';
 import 'react-toastify/dist/ReactToastify.css';
 import SignUpForm from '../user/signup/Signup';
 import {UserProvider} from "../user/UserContext";
@@ -29,100 +29,85 @@ toast.configure({
     bodyClassName: 'toastBody'
 });
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentUser: null,
-            isAuthenticated: false,
-        };
+const App = () => {
+    const [currentUser, setCurrentUser] = useState({});
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-        notification.config({
-            placement: 'topRight',
-            top: 70,
-            duration: 3,
+    useEffect(() => {
+        loadCurrentUser();
+        removeLoader();
+
+    },[]);
+
+    const loadCurrentUser = (roles) => {
+        getCurrentUser()
+            .then(response => {
+                console.log('load user response :', response);
+                response = {...response, roles};
+                setCurrentUser(response);
+                setIsAuthenticated(true);
+
+            }).catch(error => {
+            showError(error);
         });
-    }
+    };
 
-    removeLoader = () => {
+    const removeLoader = () => {
         const ele = document.getElementById('ipl-progress-indicator');
         if (ele) {
             // fade out
             ele.classList.add('available');
             ele.outerHTML = ''
         }
-    }
-    ;
-
-    loadCurrentUser = (roles) => {
-        getCurrentUser()
-            .then(response => {
-                // console.log('load user response :', response);
-                response = {...response, roles};
-                this.setState({
-                    currentUser: response,
-                    isAuthenticated: true,
-                });
-            }).catch(error => {
-            showError(error);
-        });
     };
 
-    componentDidMount() {
-        this.loadCurrentUser();
-        this.removeLoader();
-    }
 
-    handleLogout = (redirectTo = "/", notificationType = "success", description = "You're successfully logged out.") => {
+    let history = useHistory();
+
+    const handleLogout = (redirectTo = "/", notificationType = "success", description = "You're successfully logged out.") => {
         localStorage.removeItem(ACCESS_TOKEN);
-        this.setState({
-            currentUser: null,
-            isAuthenticated: false
-        });
-        this.props.history.push(redirectTo);
+        setCurrentUser(null);
+        setIsAuthenticated(false);
+        history.push(redirectTo);
         toast.success("با موفقیت خارج شدید");
     };
 
-    handleLogin = (userRole) => {
+    const handleLogin = (userRole) => {
         toast.success("با موفقیت وارد شدید");
-        // console.log('UserRole',userRole);
-        this.loadCurrentUser(userRole);
-        this.props.history.push("/suplist");
+        loadCurrentUser(userRole);
+        history.push("/suplist");
     };
 
-    render() {
-        return (
-            <Layout className="app-container">
-                <UserProvider value={this.state.currentUser}>
-                    <AppHeader isAuthenticated={this.state.isAuthenticated}
-                               currentUser={this.state.currentUser}
-                               onLogout={this.handleLogout}/>
-
-                    <Content className="app-content">
-                        <div className="container">
-                            <Switch>
-                                <Route exact path="/"
-                                       render={(props) => <Login onLogin={this.handleLogin} {...props} />}/>
-                                <Route path="/login"
-                                       render={(props) => <Login onLogin={this.handleLogin} {...props} />}/>
-                                <PrivateRoute authenticated={this.state.isAuthenticated}
-                                              handleLogout={this.handleLogout}
-                                              path="/signup/:username?" component={SignUpForm}/>
-                                <Route path="/users/:username"
-                                       render={(props) => <Profile isAuthenticated={this.state.isAuthenticated}
-                                                                   currentUser={this.state.currentUser} {...props} />}/>
-                                {
-                                    routs.map((rout) => (
-                                        <Route {...rout}/>
-                                    ))
-                                }
-                            </Switch>
-                        </div>
-                    </Content>
-                </UserProvider>
-            </Layout>
-        );
-    }
-}
+    return (
+        <Layout className="app-container">
+            <UserProvider value={currentUser}>
+                <AppHeader isAuthenticated={isAuthenticated}
+                           currentUser={currentUser}
+                           onLogout={handleLogout}/>
+                <Content className="app-content">
+                    <div className="container">
+                        <Switch>
+                            <Route exact path="/"
+                                   render={(props) => <Login onLogin={handleLogin} {...props} />}/>
+                            <Route path="/login"
+                                   render={(props) => <Login onLogin={handleLogin} {...props} />}/>
+                            <PrivateRoute authenticated={isAuthenticated}
+                                          handleLogout={handleLogout}
+                                          path="/signup/:username?" component={SignUpForm}/>
+                            <Route path="/users/:username"
+                                   render={(props) => <Profile isAuthenticated={isAuthenticated}
+                                                               currentUser={currentUser} {...props} />}/>
+                            {
+                                routs.map((rout) => (
+                                    <Route {...rout}/>
+                                ))
+                            }
+                        </Switch>
+                    </div>
+                </Content>
+            </UserProvider>
+        </Layout>
+    );
+};
 
 export default withRouter(App);
